6e7e1b8db08cbf3dec5bbeee2f6b82fd51b86b5f
Afonso Silva <afonsodolmensilva@outlook.com>
2026-05-26 14:53:47 -0300

    Detalhes estado registradores em tela azul

--- kernel/isr.c
+++ kernel/isr.c
@@ -8,11 +8,12 @@ void draw_blue_screen()
   clear();
 }
 
-void raise_error_screen(char* msg)
+void raise_error_screen(char* msg, state_registers* state)
 {
   klog_error("Kernel Panic!");
   klog_error(msg);
-  
+
+  // Desenha tela azul
   draw_blue_screen();
 
   vga_set_color(VGA_COLOR_WHITE, VGA_COLOR_BLUE);
@@ -28,14 +29,30 @@ void raise_error_screen(char* msg)
   vga_set_color(VGA_COLOR_WHITE, VGA_COLOR_BLUE);
   kprint("\n");
   kprint(msg);
+
+  // Número da exceção
+  kprintf("\nException Number: %d", (signed int)state->int_no);
+
+  // Escreve estado dos registradores
+  kprintf("\n\nEAX: %x,", state->eax);
+  kprintf(" EBX: %x,", state->ebx);
+  kprintf(" ECX: %x,", state->ecx);
+  kprintf(" EDX: %x", state->edx);
+
+  kprintf("\n\nCS: %x,", state->cs);
+  kprintf(" EIP: %x", state->eip);
+
+  // Informações da pilha
+  kprintf("\n\nESP: %x,", state->esp);
+  kprintf(" EBP: %x", state->ebp);
 }
 
 void isr_handler(state_registers* state)
 {
   switch (state->int_no)
   {
-    case 0: raise_error_screen("Division by Zero!"); break;
-    default: raise_error_screen("Unknow Exception!"); break;
+    case 0: raise_error_screen("Division by Zero!", state); break;
+    default: raise_error_screen("Unknow Exception!", state); break;
   }
 
   // Trava o processamento
